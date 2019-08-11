ee07d510bf66527f77008e0f69e55b2d03603a71
think2017 <think2017@gmail.com>
2019-08-11 09:32:31 +0800

    init

---
+++ src/Payment.php
@@ -0,0 +1,22 @@
+<?php
+namespace Avalon\Payment;
+
+class Payment
+{
+
+    protected $config = [];
+
+    public function __construct()
+    {
+        $this->initConfig();
+    }
+
+    public function initConfig()
+    {
+        $configs = config('payment', []);
+    }
+
+    public function surpriseMe($name){
+        echo $name, ", u're awesome."
+    }
+}
---
+++ src/PaymentProvider.php
@@ -0,0 +1,23 @@
+<?php
+namespace Avalon\Payment;
+
+use Illuminate\Support\ServiceProvider;
+
+class PaymentProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        if (!file_exists(config_path('payment.php'))) {
+            $this->publishes([
+                dirname(__DIR__) . '/config/payment.php' => config_path(payment),
+            ], 'config');
+        }
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            dirname(__DIR__) . '/config/payment.php', 'iimage'
+        );
+    }
+}
